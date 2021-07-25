8d0d8c3bbfa04d90aaeeb7a7ce1ffef207a6bcd1
seohyun319 <tjguselly0319@gmail.com>
2021-07-25 22:40:00 +0900

    10 Bcrypt로 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,24 @@ const userSchema = mongoose.Schema({
         type:Number
     }    
 })
+
+userSchema.pre('save', function(next) {//mongoose에서 가져온 메소드. user정보를 저장하기 전에 function 내용을 한다
+    var user = this;
+    
+    //비밀번호를 암호화시킬 것
+    if(user.isModified('password')){ //userSchema가 save될때마다 실행되는 게 아니라 password가 변경될 때 실행
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            //bcrypt에서 salt를 만듦(genSalt). 에러나면 err, 성공하면 salt 실행
+            if(err) return next(err) //에러나면 다음 err (index.js에서 register route에서 if(err) 부분) 실행
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password=hash //성공하면 user의 password를 hash(암호화)된 패스워드로 바꿈.
+                next()
+            })
+        })
+    }
+})
+
 
 const User = mongoose.model('User', userSchema)  //모델로 스키마 감싸줌
 
