eeb7e00f10bcc288c974bc6226feca1a2ca95210
seohyun319 <tjguselly0319@gmail.com>
2021-07-26 10:47:34 +0900

    29 로그인 1

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    //usestate 치면 자동완성
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems:'center',
+            width:'100%', height:'100vh'
+        }}>
+
+            <form style={{ display:'flex', flexDirection:'column'}}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={Email} onChange={onPasswordHandler}/>
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
+            
         </div>
     )
 }
